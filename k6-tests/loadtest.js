f7bd144ce58c6dc3ef92b5fcc0064eaaa0633e88
thegoodteam <kaylae@andrew.cmu.edu>
2025-10-23 15:58:23 -0400

    add clarifying comments on k6 test load

--- k6-tests/loadtest.js
+++ k6-tests/loadtest.js
@@ -3,10 +3,10 @@ import { sleep } from 'k6';
 
 export const options = {
   vus: 10, // virtual users
-  duration: '20s',
+  duration: '20s', //seconds
 };
 
 export default function () {
-  http.get('http://localhost:4567/'); // change port if your NodeBB runs elsewhere
+  http.get('http://localhost:4567/');
   sleep(1);
 }
