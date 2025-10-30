e0d19774bf0c5ca4a52a6d4da8b785276d8d1fe2
lily <lilyb@LilbeersLaptop.localdomain>
2025-10-29 23:10:54 -0400

    implementing Nicks testing method

--- k6-tests/loadtest.js
+++ k6-tests/loadtest.js
@@ -2,13 +2,8 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '10s', target: 2 }, //slow ramp-up 
-    { duration: '20s', target: 5 }, // ramp-up to 5 users
-    { duration: '40s', target: 5 }, // stay at 5 users
-    { duration: '20s', target: 0 }, // ramp-down to 0 users
-  ], 
-  
+  vus: 10, // number of virtual users
+  duration: '30s', // test duration
   thresholds: {
     http_req_duration: ['p(95)<1000'], // 95% of requests < 1s
     http_req_failed: ['rate<0.05'],    // failure rate < 5%
@@ -17,19 +12,19 @@ export const options = {
 
 };
 
+const BASE_URL = 'http://localhost:4567';
+
 export default function () {
   //test homepage
-  let res1 = http.get('http://localhost:4567/');
-  console.log('res1: ', res1.status);
-  check(res1, {
+  const res = http.get('{BASE_URL}/');
+  console.log('res1: ', res.status);
+  check(res, {
     'status is 200': (r) => r.status === 200,
     'homepage loads fast': (r) => r.timings.duration < 500,
   })
 
-  sleep(1);
-
   //test api 
-  let apiRes = http.get('http://localhost:4567/api/config');
+  let apiRes = http.get('{BASE_URL}/api/config');
   console.log ('api rest: ',apiRes.status);
   check(apiRes, {
     'status is 200': (r) => r.status === 200,
