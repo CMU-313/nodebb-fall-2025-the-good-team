622f3dbb9a12dc6e1cd500da28646890a86ce2a4
Nick <nickjj19@gmail.com>
2025-11-04 07:07:26 +0000

    translator working

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,14 +4,9 @@
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
+translatorApi.translate = async function (postData) {
+	const TRANSLATOR_API = 'http://172.17.0.2:5000';
+	const response = await fetch(TRANSLATOR_API + '/?content=' + postData.content);
+	const data = await response.json();
+	return [data.is_english, data.translated_content];
 };
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
-// };
